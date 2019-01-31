826895a33b5d671a00f8712443093ef6b1f263c7
Michelle Stewart <stew0343@algonquinlive.com>
2019-01-31 11:08:29 -0500

    Separation of concerns

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -30,18 +30,18 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+<script src="./HelloWorld.js">
+// export default {
+//   name: 'HelloWorld',
+//   props: {
+//     msg: String
+//   }
+// }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
+<style src="./HelloWorld.css" scoped>
+/* h3 {
   margin: 40px 0 0;
 }
 ul {
@@ -54,5 +54,5 @@ li {
 }
 a {
   color: #42b983;
-}
+} */
 </style>
